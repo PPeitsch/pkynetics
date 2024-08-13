9a6cdb55e7e5456c9b9a4787097c15b0da6d5a48
PabloPeitsch <pablo.peitsch@gmail.com>
2024-08-12 22:58:01 -0300

    implemented Freeman-Carroll method

--- model_fitting_methods/__init__.py
+++ model_fitting_methods/__init__.py
@@ -3,7 +3,7 @@
 from .avrami import avrami_method, avrami_equation
 from .kissinger import kissinger_method, kissinger_equation, calculate_t_p
 from .coats_redfern import coats_redfern_method, coats_redfern_equation, coats_redfern_plot
-#from .freeman_carroll import freeman_carroll_method, freeman_carroll_equation, freeman_carroll_plot
+from .freeman_carroll import freeman_carroll_method, freeman_carroll_equation, freeman_carroll_plot
 #from .horowitz_metzger import horowitz_metzger_method, horowitz_metzger_equation, horowitz_metzger_plot
 
 __all__ = [
@@ -15,9 +15,9 @@ __all__ = [
     "coats_redfern_method",
     "coats_redfern_equation",
     "coats_redfern_plot",
-#    "freeman_carroll_method",
-#    "freeman_carroll_equation",
-#    "freeman_carroll_plot",
+    "freeman_carroll_method",
+    "freeman_carroll_equation",
+    "freeman_carroll_plot",
 #    "horowitz_metzger_method",
 #    "horowitz_metzger_equation",
 #    "horowitz_metzger_plot"
