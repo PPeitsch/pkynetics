c8b1c219d021baa67bed87b86fbbbf14a8f3daca
PabloPeitsch <pablo.peitsch@gmail.com>
2024-10-10 18:09:19 -0300

    improved jmak module

--- model_fitting_methods/__init__.py
+++ model_fitting_methods/__init__.py
@@ -1,15 +1,15 @@
 """Model fitting methods for kinetic analysis in Pkynetics."""
 
-from .avrami import avrami_method, avrami_equation, calculate_half_time
+from .jmak import jmak_method, jmak_equation, jmak_half_time
 from .kissinger import kissinger_method, kissinger_equation
 from .coats_redfern import coats_redfern_method, coats_redfern_equation
 from .freeman_carroll import freeman_carroll_method, freeman_carroll_equation, plot_diagnostic
 from .horowitz_metzger import horowitz_metzger_method, horowitz_metzger_equation
 
 __all__ = [
-    "avrami_method",
-    "avrami_equation",
-    "calculate_half_time",
+    "jmak_method",
+    "jmak_equation",
+    "jmak_half_time",
     "kissinger_method",
     "coats_redfern_method",
     "coats_redfern_equation",
