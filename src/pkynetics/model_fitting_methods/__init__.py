9c82e7f6fe0c74aee1416a469a4dc402c67bb843
PabloPeitsch <pablo.peitsch@gmail.com>
2024-11-27 22:14:21 -0300

    [INFRA] Organize model_fitting_methods exports
    - Group methods by type in __all__
    - Add descriptive comments for each method group
    - Sort methods alphabetically within groups
    
    This improves code organization and maintainability.

--- src/pkynetics/model_fitting_methods/__init__.py
+++ src/pkynetics/model_fitting_methods/__init__.py
@@ -17,18 +17,23 @@ from .jmak import (
 from .kissinger import kissinger_equation, kissinger_method
 
 __all__ = [
-    "jmak_method",
+    # Coats-Redfern methods
+    "coats_redfern_equation",
+    "coats_redfern_method",
+    # Freeman-Carroll methods
+    "freeman_carroll_equation",
+    "freeman_carroll_method",
+    "plot_diagnostic",
+    # Horowitz-Metzger methods
+    "horowitz_metzger_equation",
+    "horowitz_metzger_method",
+    # JMAK methods
+    "fit_modified_jmak",
     "jmak_equation",
     "jmak_half_time",
+    "jmak_method",
     "modified_jmak_equation",
-    "fit_modified_jmak",
-    "kissinger_method",
-    "coats_redfern_method",
-    "coats_redfern_equation",
-    "freeman_carroll_method",
-    "freeman_carroll_equation",
-    "plot_diagnostic",
-    "horowitz_metzger_method",
-    "horowitz_metzger_equation",
+    # Kissinger methods
     "kissinger_equation",
+    "kissinger_method",
 ]
